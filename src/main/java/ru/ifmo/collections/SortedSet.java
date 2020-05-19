df85aa4266ed5dc6f61d47763a18ff03f570f780
Vladislav <vlad@grokhotov.ru>
2020-05-20 02:13:18 +0300

    added SortedSet implementation

--- src/main/java/ru/ifmo/collections/SortedSet.java
+++ src/main/java/ru/ifmo/collections/SortedSet.java
@@ -1,36 +1,142 @@
 package ru.ifmo.collections;
 
-import java.util.AbstractSet;
-import java.util.Comparator;
+
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.IntFunction;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 /**
  * Represents sorted set of unique values.
  * create() returns a SortedSet instance with natural ordering. (i.e. from smallest to largest in case of integer numbers)
  * from() is used to create a SortedSet instance with given Comparator.
  * Instances of a class can be created using only these two methods above.
- *
+ * <p>
  * This class should not be abstract and should be capable of adding/removing single/multiple elements.
  * It has two more methods: getSorted() and getReversed() which return an array of set contents in forward and reverse order respectively.
- *
+ * <p>
  * NB! This class must have only map(s) as an internal data structure(s).
  *
  * @param <T> set contents type
  */
-public abstract class SortedSet<T> extends AbstractSet<T> {
-    // private final Map<???, ???> contents; TODO decide Map implementation and key/value types. "???" is used just as an example
+public final class SortedSet<T> extends AbstractSet<T> {
+    private final Map<T, Integer> contents;
+
     public static <T> SortedSet<T> create() {
-        throw new UnsupportedOperationException(); // TODO implement
+        return from(null);
     }
 
     public static <T> SortedSet<T> from(Comparator<T> comparator) {
-        throw new UnsupportedOperationException(); // TODO implement
+        return new SortedSet<>(comparator);
     }
 
-    public T[] getSorted() {
-        throw new UnsupportedOperationException(); // TODO implement
+    private SortedSet(Comparator<T> comparator) {
+        contents = new TreeMap<>(comparator);
     }
 
-    public T[] getReversed() {
-        throw new UnsupportedOperationException(); // TODO implement
+    public List<T> getSorted() {
+        return new ArrayList<>(contents.keySet());
+    }
+
+    public List<T> getReversed() {
+        var list = getSorted();
+        Collections.reverse(list);
+        return list;
+    }
+
+    @Override
+    public boolean add(T t) {
+        boolean isInContents = contents.containsKey(t);
+        if (!isInContents) {
+            contents.put(t, 1);
+        }
+        return isInContents;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        boolean hasChanged = false;
+        for (var item : c) {
+            if (add(item)) {
+                hasChanged = true;
+            }
+        }
+        return hasChanged;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        boolean isInContents = contents.containsKey(o);
+        if (isInContents) {
+            contents.remove(o);
+        }
+        return isInContents;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        boolean hasChanged = false;
+        for (var item : c) {
+            if (remove(item)) {
+                hasChanged = true;
+            }
+        }
+        return hasChanged;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return contents.isEmpty();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return contents.containsKey(o);
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return contents.keySet().containsAll(c);
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return contents.keySet().iterator();
+    }
+
+    @Override
+    public void forEach(Consumer<? super T> action) {
+        contents.keySet().forEach(action);
+    }
+
+    @Override
+    public <T1> T1[] toArray(IntFunction<T1[]> generator) {
+        return contents.keySet().toArray(generator);
+    }
+
+    @Override
+    public boolean removeIf(Predicate<? super T> filter) {
+        return contents.keySet().removeIf(filter);
+    }
+
+    @Override
+    public Spliterator<T> spliterator() {
+        return contents.keySet().spliterator();
+    }
+
+    @Override
+    public Stream<T> stream() {
+        return contents.keySet().stream();
+    }
+
+    @Override
+    public Stream<T> parallelStream() {
+        return contents.keySet().parallelStream();
+    }
+
+    @Override
+    public int size() {
+        return contents.size();
     }
 }
